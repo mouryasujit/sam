c7eba1c1b50d81c1411887f8abb9762249608993
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-10-17 22:34:14 +0530

    Added conditions

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,10 +19,12 @@ export default function Navbar() {
             <h1 className="uppercase font-bold text-3xl">SAM</h1>
 
             <nav className="flex flex-row gap-4 text-xl">
-                <div className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-2.5 text-center inline-flex items-center">
-                    <h1 className="">Name of the user</h1>
-                </div>
-
+                {user && (
+                    <div className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-2.5 text-center inline-flex items-center">
+                        <h1 className="">Name of the user</h1>
+                    </div>
+                )}
+                
                 {user ? (
                     <button
                         className="border rounded-lg px-2 py-1 border-black"
